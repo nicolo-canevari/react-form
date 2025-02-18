fa0f9e7e7ad76bbe90f0505a6af8bb24925f2b08
nicolo_canevari <nico.canevari@outlook.it>
2025-02-18 15:59:13 +0100

    [FEAT] prima parte del 1st milestone

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,18 @@
+const articles = [
+
+    { id: 1, title: 'Articolo 1' },
+    { id: 2, title: 'Articolo 2' },
+    { id: 3, title: 'Articolo 3' },
+    { id: 1, title: 'Articolo 4' },
+    { id: 2, title: 'Articolo 5' },
+    { id: 3, title: 'Articolo 6' },
+
+];
+
+const ArticleList = () => {
+
+
+
+};
+
+export default ArticleList;
